261c05e56fec3483cd067b9ecc2b0b48709c09a9
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-03-28 23:27:28 +0300

    Add background service and register it

--- src/pp_bot.Server/Program.cs
+++ src/pp_bot.Server/Program.cs
@@ -3,7 +3,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using pp_bot.Server.Services;
 using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
 
 namespace pp_bot.Server
 {
@@ -17,6 +19,8 @@ namespace pp_bot.Server
                     var config = context.Configuration;
                     services.AddSingleton<ITelegramBotClient>(
                         new TelegramBotClient(config["BOT_TOKEN"]));
+                    services.AddSingleton<IUpdateHandler, BotHandler>();
+                    services.AddHostedService<BotHandlerService>();
                 })
                 .Build();
             //await host.RunAsync();
