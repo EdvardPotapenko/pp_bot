dfb50a67e4a5f01dfb91d29b239b982e2fc4fd2b
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-03-28 23:38:43 +0300

    Add configuration files, add db context to services, configure variables

--- src/pp_bot.Server/Program.cs
+++ src/pp_bot.Server/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using pp_bot.Server.model;
 using pp_bot.Server.Services;
 using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;
@@ -14,6 +17,19 @@ namespace pp_bot.Server
         private static async Task Main()
         {
             var host = new HostBuilder()
+                .ConfigureHostConfiguration(builder =>
+                {
+                    builder.AddEnvironmentVariables();
+                })
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    var env = context.HostingEnvironment.EnvironmentName;
+                    builder
+                        .AddJsonFile("botsettings.json", false, false)
+                        .AddJsonFile($"botsettings.{env}.json", true, false)
+                        .AddJsonFile("dbsettings.json", false, false)
+                        .AddJsonFile($"dbsettings.{env}.json", true, false);
+                })
                 .ConfigureServices((context, services) =>
                 {
                     var config = context.Configuration;
@@ -21,6 +37,8 @@ namespace pp_bot.Server
                         new TelegramBotClient(config["BOT_TOKEN"]));
                     services.AddSingleton<IUpdateHandler, BotHandler>();
                     services.AddHostedService<BotHandlerService>();
+                    services.AddDbContext<PP_Context>(options => options
+                        .UseNpgsql(config.GetConnectionString("DB_CONN_STR")));
                 })
                 .Build();
             //await host.RunAsync();
