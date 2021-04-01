5521ba169d245e12a790f54afc14c15c272bc137
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-04-01 20:07:08 +0300

    Refactoring: Delete Chat.Id property (we don't need it) #6

--- src/pp_bot.Server/Migrations/20210401170458_RemoveIdFieldForChat.Designer.cs
+++ src/pp_bot.Server/Migrations/20210401170458_RemoveIdFieldForChat.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using pp_bot.Server.Models;
@@ -9,9 +10,10 @@ using pp_bot.Server.Models;
 namespace pp_bot.Server.Migrations
 {
     [DbContext(typeof(PP_Context))]
-    partial class PP_ContextModelSnapshot : ModelSnapshot
+    [Migration("20210401170458_RemoveIdFieldForChat")]
+    partial class RemoveIdFieldForChat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/pp_bot.Server/Models/Chat.cs
+++ src/pp_bot.Server/Models/Chat.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace pp_bot.Server.Models {
-    public class Chat {
-        public int Id{get;init;}
-        public long ChatId {get;init;}
-        public string ChatName{get;init;}
-        public List<BotUser> ChatUsers{get;set;} = new List<BotUser>();
+namespace pp_bot.Server.Models
+{
+    public sealed record Chat
+    {
+        [Key]
+        public long ChatId { get; init; }
+        public string ChatName{ get; init; }
+        public List<BotUser> ChatUsers{ get; init; }
     }
 }
